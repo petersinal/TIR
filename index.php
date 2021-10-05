9e3c16102d81e142eb59ff7fb9ae9b721246548a
Marek Kubala <you@example.com>
2021-10-05 10:36:00 +0200

    start

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "vertrigo";
+$dbname = "prax";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql = "SELECT meno, nadpis, obsah FROM pages";
+
+$result = $conn->query($sql);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+</head>
+
+<body>
+
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname"><br>
+    <input type="submit" value="Submit">
+
+    <?php
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "Meno: " . $row["meno"]. " E-mail: " . $row["nadpis"]. " Obsah: " . $row["obsah"];
+        }
+      } else {
+        echo "0 results";
+      }
+    ?>
+</body>
+
+</html>
