481f73ae03862265d6a4d301bc032b1a3d256796
Peter Sinal <you@example.com>
2021-11-16 11:58:12 +0100

    Added login with md5

--- TIR/admin/assets/db.php
+++ TIR/admin/assets/db.php
@@ -7,18 +7,9 @@ $dbname = "tir_sinal";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-$sql = "SELECT id, login, heslo, rola FROM uzivatelia";
-$result = $conn->query($sql);
-
-while($row = $result->fetch_assoc()) {
-    $name = $row['login'];
-    $password = $row['heslo'];
-    $post = $row['rola'];
-  }
-
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
 ?>
--- TIR/admin/theme/admin/login.php
+++ TIR/admin/theme/admin/login.php
@@ -3,19 +3,35 @@
 <?php
     $inputname = $_POST['name'];
     $inputpassword = $_POST['password'];
+    $name = [];
     $prihlasenyuzivazel = false;
     $riadkysuboru = file('users.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($riadkysuboru as $riadok)
-    {
-        if($inputname == $name && $inputpassword == $password){
+
+    $sql = "SELECT id, login, heslo, rola FROM uzivatelia WHERE login ='". $inputname."'";
+    $result = $conn->query($sql);
+
+    while($row = $result->fetch_assoc()){
+        if($inputname == $row['login'] && md5($inputpassword) == $row['heslo']){
             $_SESSION["name"] = $inputname;
             $_SESSION["password"] = $inputpassword;
             $_SESSION["post"] = $post;
             $prihlasenyuzivazel = true;
             header('Location: ../domov');
             die();
-        }
     }
+}
+    
+    // foreach ($riadkysuboru as $riadok)
+    // {
+    //     if($inputname == $name && $inputpassword == $password){
+    //         $_SESSION["name"] = $inputname;
+    //         $_SESSION["password"] = $inputpassword;
+    //         $_SESSION["post"] = $post;
+    //         $prihlasenyuzivazel = true;
+    //         header('Location: ../domov');
+    //         die();
+    //     }
+    // }
     if($prihlasenyuzivazel == false){
         echo '<p style="color: red">Nesprávne meno alebo heslo</p>';
         echo '<a href="index.php">Naspäť</a>';
